5667699e1e759a36d3d4aeafdbd84b271388dec7
Harsh <harsh.harshvardhan@visium.ch>
2021-01-16 20:43:30 +0100

    modified actions so that only hugo site is built

--- action.sh
+++ action.sh
@@ -3,24 +3,8 @@
 set -e
 set -o pipefail
 
-if [[ -n "${TOKEN}" ]]; then
-    GITHUB_TOKEN=${TOKEN}
-fi
 
-if [[ -z "${GITHUB_TOKEN}" ]]; then
-    echo "Set the GITHUB_TOKEN env variable."
-    exit 1
-fi
 
-if [[ -z "${TARGET_REPO}" ]]; then
-    echo "Set the TARGET_REPO env variable."
-    exit 1
-fi
-
-if [[ -z "${TARGET_BRANCH}" ]]; then
-    TARGET_BRANCH=main
-    echo "No TARGET_BRANCH was set, so defaulting to ${TARGET_BRANCH}"
-fi
 
 if [[ -z "${HUGO_VERSION}" ]]; then
     HUGO_VERSION=0.80.0
@@ -42,39 +26,5 @@ curl -sSL "${URL}" > /tmp/hugo.deb && dpkg --force architecture -i /tmp/hugo.deb
 
 echo "Building the Hugo site with: 'hugo ${HUGO_ARGS}'"
 hugo "${HUGO_ARGS}"
-
-TARGET_REPO_URL="https://${GITHUB_TOKEN}@github.com/${TARGET_REPO}.git"
-
-rm -rf .git
-cd public
-
-if [[ -n "${CNAME}" ]]; then
-    echo "CNAME set to ${CNAME}, creating file CNAME"
-    echo "${CNAME}" > CNAME
-fi
-
-echo "Committing the site to git and pushing"
-
-git init
-
-if ! git config --get user.name; then
-    git config --global user.name "Harsh"
-fi
-
-if ! git config --get user.email; then
-    git config --global user.email "$harshv834@gmail.com"
-fi
-
-echo "Getting hash for base repository commit"
-HASH=$(echo "${GITHUB_SHA}" | cut -c1-7)
-
-# Now add all the changes and commit and push
-if [[ "${TARGET_BRANCH}" != "main" ]]; then
-  git checkout -b ${TARGET_BRANCH}
-fi
-
-git add . && \
-git commit -m "Auto publishing site from ${GITHUB_REPOSITORY}@${HASH}" && \
-git push --force "${TARGET_REPO_URL}" ${TARGET_BRANCH}
 
 echo "Complete"
